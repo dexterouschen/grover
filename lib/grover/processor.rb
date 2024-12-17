02d25e546c73a7ea2d9ddeb63ba0068d49826963
Dexter Chen <jchen@encentivenergy.com>
2024-12-17 18:05:25 -0500

    revert puppeteer 23+ fix

--- lib/grover/processor.rb
+++ lib/grover/processor.rb
@@ -21,7 +21,7 @@ class Grover
       return unless result
       return result if result.is_a?(String)
 
-      (result['data'].presence || result.values).pack('C*')
+      result['data'].pack('C*')
     ensure
       cleanup_process if stdin
     end
